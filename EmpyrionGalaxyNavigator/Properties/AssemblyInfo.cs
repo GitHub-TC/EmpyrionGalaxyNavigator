28163950ce21d79b9640ef62f1eabf8c4df4c565
ASTIC <github@cl-mail.eu>
2019-09-14 17:46:38 +0200

    Infos

--- EmpyrionGalaxyNavigator/Properties/AssemblyInfo.cs
+++ EmpyrionGalaxyNavigator/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("EmpyrionGalaxyNavigator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ASTIC/TC")]
 [assembly: AssemblyProduct("EmpyrionGalaxyNavigator")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
